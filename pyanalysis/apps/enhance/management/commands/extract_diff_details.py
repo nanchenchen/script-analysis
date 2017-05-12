0af9202d141558606122ec6ff86ab7704516f7ac
nanchenchen <nanchen.chen@gmail.com>
2017-05-12 00:28:13 -0700

    tokenize diff lines

--- pyanalysis/apps/enhance/management/commands/extract_diff_details.py
+++ pyanalysis/apps/enhance/management/commands/extract_diff_details.py
@@ -1,11 +1,25 @@
-from django.core.management.base import BaseCommand, make_option, CommandError
-from time import time
 import path
+from StringIO import StringIO
+from time import time
+import tokenize
+
+from django.core.management.base import BaseCommand, make_option, CommandError
 from django.db import transaction
 
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+def tokenize_line(line):
+
+    tokens = tokenize.generate_tokens(StringIO(line).readline)
+    tokens_list = []
+    for token in tokens:
+        type = tokenize.tok_name[token[0]]
+        text = token[1]
+        tokens_list.append((type, text))
+    return tokens_list
 
 class Command(BaseCommand):
     help = "Run gensim operations."
@@ -28,7 +42,7 @@ class Command(BaseCommand):
         threshold = options.get('threshold')
 
         from pyanalysis.apps.enhance.models import ScriptDiff
-        diffs = ScriptDiff.objects.all()[0]
+        diffs = ScriptDiff.objects.all()[1]
 
         # Step 1: Extract + and - lines
         diff_text = diffs.text
@@ -37,11 +51,16 @@ class Command(BaseCommand):
         diff_lines_minus = filter(lambda x: x.startswith('-'), diff_lines)
         diff_lines_plus = [x[1:] for x in diff_lines_plus]   # remove + sign
         diff_lines_minus = [x[1:] for x in diff_lines_minus] # remove - sign
-        print diff_lines_plus
-        print diff_lines_minus
+
+        # Step 1.5: Tokenize
+        diff_lines_plus_t = [tokenize_line(line) for line in diff_lines_plus]   # tokenize
+        diff_lines_minus_t = [tokenize_line(line) for line in diff_lines_minus]   # tokenize
+        print diff_lines_plus_t
+        print diff_lines_minus_t
 
 
-
+        import pdb
+        pdb.set_trace()
         # bulk_diff = []
         # for pair in pairs:
         #     diff_content = pair.get_diff()
